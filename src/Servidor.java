b21b778f0b93ab25eba2513a0fa6f632d4132ddc
HarounDominique <haroundominique@gmail.com>
2023-03-29 21:28:56 +0200

    añadiendo streams de salida (hacia cliente desde servidor) y de entrada (hacia servidor desde cliente)

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,11 +12,28 @@ public class Servidor {
             throw new RuntimeException(e);
         }
         System.out.println("En escucha en el puerto "+servidor.getLocalPort());
+        Socket cliente = null;
         try{
-            Socket cliente = servidor.accept(); //esperando a un cliente
+            cliente = servidor.accept(); //esperando a un cliente
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        //Creando stream de salida:
+        OutputStream salida=null;
+        try {
+            salida = cliente.getOutputStream();
+        }catch (IOException el) {
+            el.printStackTrace() ;
+        }
+        DataOutputStream flujoSalida = new DataOutputStream(salida);
+        //Creando stream de entrada:
+        InputStream entrada=null;
+        try {
+            entrada = cliente.getInputStream() ;
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        DataInputStream flujoEntrada = new DataInputStream(entrada);
         try {
             servidor.close(); //cerrando el socket servidor
         } catch (IOException e) {
