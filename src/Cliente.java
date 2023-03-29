e9e586833a45db2556200e28129ec4ac2bff1be8
HarounDominique <haroundominique@gmail.com>
2023-03-29 21:48:00 +0200

    NOTA: en el servidor para levantar la tubería hacia cliente se hace a partir de Socket (no ServerSocket); y en el cliente se hace TAMBIÉN a través de Socket, esto es, desde sí mismo.

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,8 @@
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Cliente {
@@ -18,6 +21,21 @@ public class Cliente {
         System.out.println("Puerto Remoto: "+Cliente.getPort());//devuelve el puerto remoto
         System.out.println("Host Remoto: "+i.getHostName().toString());//devuelve el nombre del host, que en este caso es 'localhost'
         System.out.println("IP Host Remoto: "+i.getHostAddress().toString());
+
+        //Creando stream de entrada:
+        InputStream entrada=null;
+        ServerSocket servidor = null;
+        try {
+            entrada = servidor.getInputStream() ;
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        DataInputStream flujoEntrada = new DataInputStream(entrada);
+        //leemos el DIS flujoEntrada mientras tenga un contenido que mostrar
+        while (flujoEntrada.available() > 0) {
+            System.out.println(flujoEntrada.readUTF());
+        }
+        flujoEntrada.close(); //cerramos el DIS
         try {
             Cliente.close();// Cierra el socket
         } catch (IOException e) {
