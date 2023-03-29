29e03fddf29aee275191b291602fb7452031c5bb
HarounDominique <haroundominique@gmail.com>
2023-03-29 21:39:34 +0200

    Lado del servidor completo (y en principio, correcto)

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,3 +1,27 @@
-public class Cliente {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class Cliente {
+    public static void main(String[] args) {
+        String Host = "localhost";
+        int Puerto = 6000;//puerto remoto
+        // ABRIR SOCKET
+        Socket Cliente = null;//conecta
+        try {
+            Cliente = new Socket(Host, Puerto);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        InetAddress i = Cliente.getInetAddress();
+        System.out.println("Puerto local: "+Cliente.getLocalPort());//devuelve el puerto local
+        System.out.println("Puerto Remoto: "+Cliente.getPort());//devuelve el puerto remoto
+        System.out.println("Host Remoto: "+i.getHostName().toString());//devuelve el nombre del host, que en este caso es 'localhost'
+        System.out.println("IP Host Remoto: "+i.getHostAddress().toString());
+        try {
+            Cliente.close();// Cierra el socket
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,9 +1,10 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Servidor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int puerto = 6000; //el puerto al que enlazamos el socket servidor (el puerto local en este caso)
         ServerSocket servidor = null;
         try{
@@ -18,6 +19,7 @@ public class Servidor {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
         //Creando stream de salida:
         OutputStream salida=null;
         try {
@@ -26,6 +28,15 @@ public class Servidor {
             el.printStackTrace() ;
         }
         DataOutputStream flujoSalida = new DataOutputStream(salida);
+        Scanner scan = new Scanner(System.in);
+        String mensaje = scan.nextLine();//almacenamos en una variable mensaje aquello que queremos enviar a cliente
+        try {
+            flujoSalida.writeUTF(mensaje);//enviamos el mensaje a través del DOS
+            flujoSalida.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         //Creando stream de entrada:
         InputStream entrada=null;
         try {
@@ -34,6 +45,11 @@ public class Servidor {
             e.printStackTrace();
         }
         DataInputStream flujoEntrada = new DataInputStream(entrada);
+        //leemos el DIS flujoEntrada mientras tenga un contenido que mostrar
+        while (flujoEntrada.available() > 0) {
+            System.out.println(flujoEntrada.readUTF());
+        }
+        flujoEntrada.close(); //cerramos el DIS
         try {
             servidor.close(); //cerrando el socket servidor
         } catch (IOException e) {
